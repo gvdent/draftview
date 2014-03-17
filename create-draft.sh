22e7babd1a617bd59e8bf1c25917b3178362594e
Gijs van der Ent <gvdent@gmail.com>
2014-03-17 18:00:18 +0100

    Adds card downloading, filename based on grid position

--- create-draft.sh
+++ create-draft.sh
@@ -11,6 +11,11 @@ fi;
 
 for pl in {1..8}; do
   for pi in {1..4}; do
-    echo "${URL}&player=${pl}&pack=1&pick=${pi}&showpick=true"
+    CARDURL=$(curl --silent "${URL}&player=${pl}&pack=1&pick=${pi}&showpick=true" | grep "class='pickedcardimage'" | sed -e "s/.*src='\([^']*\)'.*/\1/")
+
+    LOCALFILE=card-$(( ( ( ${pl} + ${pi} - 2 ) % 8) + ( ( ${pi} - 1) * 8) )).jpg
+    echo ${LOCALFILE}
+
+    curl --silent ${CARDURL} > ${LOCALFILE}
   done
 done
