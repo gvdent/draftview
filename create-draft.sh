4359676e8678818dd64807b8b009bff65301d889
Gijs van der Ent <gvdent@gmail.com>
2014-03-17 18:16:07 +0100

    Makes card numbers 1 based

--- create-draft.sh
+++ create-draft.sh
@@ -13,7 +13,7 @@ for pl in {1..8}; do
   for pi in {1..4}; do
     CARDURL=$(curl --silent "${URL}&player=${pl}&pack=1&pick=${pi}&showpick=true" | grep "class='pickedcardimage'" | sed -e "s/.*src='\([^']*\)'.*/\1/")
 
-    LOCALFILE=card-$(( ( ( ${pl} + ${pi} - 2 ) % 8) + ( ( ${pi} - 1) * 8) )).jpg
+    LOCALFILE=card-$(( ( ( ${pl} + ${pi} - 2 ) % 8) + ( ( ${pi} - 1) * 8) + 1 )).jpg
     echo ${LOCALFILE}
 
     curl --silent ${CARDURL} > ${LOCALFILE}
