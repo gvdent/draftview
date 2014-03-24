7c9304dee218c8ed9de19cf56c345e6d44c6b890
Gijs van der Ent <gvdent@gmail.com>
2014-03-24 11:07:49 +0100

    Fixed id-finding script

--- find_ids.sh
+++ find_ids.sh
@@ -2,19 +2,23 @@
 
 DATEFORMAT="+%-m_%-d_%Y"
 
-current=$(date)
+if [[ $# > 0 ]]; then
+  current="$1"
+else
+  current=$(date)
+fi;
 
 while true; do
   formatted=$(date --date="${current}" ${DATEFORMAT})
 
   i=1
-  failed=0
-  until [ not ${good} ]; do
+  good=1
+  until [[ ${good} == 0 ]]; do
     id="${formatted}_${i}" 
     curl --silent http://gatherer.wizards.com/magic/draftools/draftviewer.asp?draftid=${id}| grep XML &> /dev/null
     good=$?
-    
-    if [[ ${good} ]]; then
+
+    if [[ ${good} > 0 ]]; then
       echo ${id}
     fi
 
